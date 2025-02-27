d8d93ea8d8ead92f9e65bcc8dd2318f06dca7969
Ankush <singhankush06@gmail.com>
2025-02-27 19:19:13 +0530

    unit test cases updated

---
+++ test/test_operation.py
@@ -0,0 +1,17 @@
+from src.math_operation import add,subtract
+
+# Define the working directory
+def test_add():
+    result = add(5, 3)
+    assert result == 8, f"Expected 8, got {result}"
+
+    result = add(-2, 10)
+    assert result == 8, f"Expected 8, got {result}"
+
+def test_subtract():
+    result = subtract(10, 5)
+    assert result == 5, f"Expected 5, got {result}"
+
+    result = subtract(0, 10)
+    assert result == -10, f"Expected -10, got {result}"
+
